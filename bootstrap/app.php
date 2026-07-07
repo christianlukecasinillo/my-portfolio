c86591e3a5e23ccb412a02bc30c20d450e6d870f
christianlukecasinillo <158162441+christianlukecasinillo@users.noreply.github.com>
2026-07-07 15:55:51 +0800

    Trust Railway proxy for HTTPS/session handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,8 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -13,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
